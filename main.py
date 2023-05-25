7c9ede2d999669c0a6e7fe7434cdc010bf515c23
Alois <sonya.shu2107@gmail.com>
2023-05-25 20:04:33 +0300

    semi-final wersion

--- main.py
+++ main.py
@@ -16,26 +16,34 @@ warnings.filterwarnings("ignore", category=DeprecationWarning)
 
 
 if __name__ == '__main__':
-    sys.stdout = tempfile.TemporaryFile()
-    sys.stderr = tempfile.TemporaryFile()
-    try:
-        os.mkdir("./text_res")
-    except OSError as error:
-        pass
-    CNN()
-    LENG()
     app = QApplication(sys.argv)
-    _id = QFontDatabase.addApplicationFont("./Styles/AdventPro-VariableFont_wdth,wght.ttf")
-    win = GuiWindow()
-    custom_font = QFont('Advent Pro', int(win.height_p*0.02))
-    app.setFont(custom_font)
-    with open("./Styles/style.css", 'r') as file:
-        sheet = file.read()
-    app.setStyleSheet(sheet)
-    choose = EnterDataDialog(win)
-    if not choose.exec_():
-        sys.exit(0)
-    win.set_data(choose)
-    win.show()
-    sys.exit(app.exec_())
+    try:
+        sys.stdout = tempfile.TemporaryFile()
+        sys.stderr = tempfile.TemporaryFile()
+        try:
+            os.mkdir("./text_res")
+        except OSError as error:
+            pass
+        CNN()
+        LENG()
+        _id = QFontDatabase.addApplicationFont("./Styles/AdventPro-VariableFont_wdth,wght.ttf")
+        win = GuiWindow()
+        custom_font = QFont('Advent Pro', int(win.height_p*0.02))
+        app.setFont(custom_font)
+        with open("./Styles/style.css", 'r') as file:
+            sheet = file.read()
+        app.setStyleSheet(sheet)
+        choose = EnterDataDialog(win)
+        if not choose.exec_():
+            sys.exit(0)
+        win.set_data(choose)
+        win.show()
+        sys.exit(app.exec_())
+    except Exception as e:
+        msg = QMessageBox()
+        msg.setIcon(QMessageBox.Critical)
+        msg.setText("Error")
+        msg.setInformativeText(e)
+        msg.setWindowTitle("Error")
+        msg.exec_()
 
